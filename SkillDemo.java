1c08d0273774ea28292956d06127ce801955fe27
ChooChooChao <nathanchao318@gmail.com>
2022-02-04 12:50:01 -0800

    Lib + test that fails

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,10 @@
-public class SkillDemo {}
+public class SkillDemo {
+   public static int factorial(int num) {
+       int result = 0;
+       for(int i = num; i > 0; i--) {
+           result *= i;
+       }
+
+       return result;
+   }
+} 
